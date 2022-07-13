64864a2c3250e0adc6206e92dac37ba50ace7261
bayuTiger <bayutoranosuke@gmail.com>
2022-07-13 10:11:23 +0900

    add: コンポーネントの準備

--- umarche/routes/web.php
+++ umarche/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ComponentTestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/component-test1', [ComponentTestController::class, 'showComponent1']);
+Route::get('/component-test2', [ComponentTestController::class, 'showComponent2']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
