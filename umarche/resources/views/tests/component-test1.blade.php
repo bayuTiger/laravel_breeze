7ff917f31a246ca90194fd71df7b5c61ded5b4ce
bayuTiger <bayutoranosuke@gmail.com>
2022-07-14 12:19:34 +0900

    add: 属性バッグ

--- umarche/resources/views/tests/component-test1.blade.php
+++ umarche/resources/views/tests/component-test1.blade.php
@@ -7,5 +7,7 @@
 
     <x-tests.card title="タイトル1" content="本文1" :message="$message" />
     <x-tests.card title="タイトル２" />
+    <x-tests.card title="CSSを親側で変更したい" class="bg-red-300" />
+
 
 </x-tests.app>
