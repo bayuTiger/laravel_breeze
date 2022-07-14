39b524fa419b59a7e181795468e39cc8fac264f3
bayuTiger <bayutoranosuke@gmail.com>
2022-07-14 15:37:15 +0900

    add: クラスベースで初期値と属性を設定

--- umarche/resources/views/tests/component-test2.blade.php
+++ umarche/resources/views/tests/component-test2.blade.php
@@ -3,5 +3,7 @@
     ヘッダー２
   </x-slot>
   test2
-  <x-test-class-base />
+  <x-test-class-base classBaseMessage="クラスベースメッセージです" />
+  <div class="mb-4"></div>
+  <x-test-class-base classBaseMessage="クラスベースメッセージです" defaultMessage="初期値から変更しました" />
 </x-tests.app>
