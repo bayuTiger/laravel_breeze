256c7a671d23a7a79f541c17560558ff10be4de7
bayuTiger <bayutoranosuke@gmail.com>
2022-07-15 23:39:14 +0900

    サービスコンテナのテスト

---
+++ umarche/memo.md
@@ -0,0 +1,23 @@
+# メモ
+
+## ライフサイクル
+
+webサーバーがpublic/index.phpにリダイレクト
+
+1. autoload読み込み
+   1. require無しで別ファイルのクラスを利用可能(namespace, use)
+2. Applicationインスタンス作成(サービスコンテナ)
+   1. Bootstrap/app.phpを読み込み
+   2. `$app = new Illuminate\Foundation\Application` Applicationインスタンスが通称**サービスコンテナ**
+3. HttpKernelインスタンス作成
+4. Requestインスタンス作成
+5. HttpKernelがリクエストを処理してResponse取得
+6. レスポンス送信
+7. terminate()で後片付け
+
+## サービスコンテナ
+
+- `dd(app());`で内容を確認できる
+- `#binding`に紐づけられているサービス一覧が格納されている
+- サービスコンテナを使うと**インスタンス化する必要がない**
+- 依存関係も解決してくれるので便利
--- umarche/routes/web.php
+++ umarche/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ComponentTestController;
+use App\Http\Controllers\LifeCycleTestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,6 +21,7 @@ Route::get('/', function () {
 
 Route::get('/component-test1', [ComponentTestController::class, 'showComponent1']);
 Route::get('/component-test2', [ComponentTestController::class, 'showComponent2']);
+Route::get('/servicecontainertest', [LifeCycleTestController::class, 'showServiceContainerTest']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
